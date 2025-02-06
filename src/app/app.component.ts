12f434bde4fdc82ffdcab9917a568abfc7397da7
EduardoYaguar <eduardoyaguarf@gmail.com>
2025-02-06 10:07:17 -0500

    Added interface and resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactoComponent } from './shared/contacto/contacto.component';
 import { CardsComponent } from './shared/cards/cards.component';
+import { RecursosService } from './servicios/recursos.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ContactoComponent, CardsComponent],
+  imports: [RouterOutlet, ContactoComponent, CardsComponent, HttpClientModule],
+  providers:[RecursosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ActividadAngular';
+  constructor(private recursosService: RecursosService){
+
+  }
 }
--- src/app/shared/cards/cards.component.ts
+++ src/app/shared/cards/cards.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Foto } from '../../interfaz/foto';
+import { RecursosService } from '../../servicios/recursos.service';
 
 @Component({
   selector: 'app-cards',
@@ -8,6 +10,7 @@ import { Component } from '@angular/core';
 })
 export class CardsComponent {
   // This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+  photos: Foto[] = [];
   cards: any[] = [
     {'name':'Leon', 'content': 'El rey de la selva, símbolo de fuerza y valentía. Vive en manadas y es un experto cazador.'},
     {'name':'Elefante', 'content': 'El gigante de la sabana. Inteligente y sociable, con una memoria prodigiosa y una trompa '},
@@ -18,5 +21,11 @@ export class CardsComponent {
     {'name':'Panda', 'content': 'El tierno comedor de bambú. Un símbolo de paz que pasa la mayor parte del día comiendo y descansando.'},
     {'name':'Cebra', 'content': 'Rayas en blanco y negro que la hacen única. Vive en grupos y su piel la ayuda a camuflarse y confundir a los depredadores.'},
     {'name':'Colibri', 'content': 'Pequeño pero veloz. Su aleteo es tan rápido que parece flotar en el aire mientras bebe néctar de las flores.'},
-  ]
+  ];
+
+  constructor(private recursosService:RecursosService ){
+    recursosService.obtenerDatos().subscribe(respuesta => {
+      this.photos = respuesta as Array<Foto>
+    })
+  }
 }
